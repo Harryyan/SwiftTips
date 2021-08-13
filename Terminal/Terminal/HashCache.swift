bfed2231bec08e4094439172d9c73d797096e9bc
harry yan <harryyan1238@gmail.com>
2021-08-14 07:18:21 +1200

    test running project

---
+++ Terminal/Terminal/HashCache.swift
@@ -0,0 +1,121 @@
+//
+//  HashCache.swift
+//  HashCache
+//
+//  Created by Harry Yan on 11/08/21.
+//
+
+import CryptoKit
+import Foundation
+
+class HashCache {
+    private(set) var hashes = [Int: String]()
+    
+    func addHash(for number: Int) async {
+        let string = SHA512.hash(data: Data(String(number).utf8)
+        ).description
+
+        hashes[number] = string
+        print(hashes)
+    }
+    
+    func compute() async {
+        print("start")
+        
+        for i in 0 ..< 10 {
+            let test = Task {
+                print(i)
+            }
+            
+            // keep order
+            print(await test.value)
+        }
+        
+        //var test2 = await test.value
+        
+        print("end")
+        
+        
+        //        DispatchQueue.concurrentPerform(iterations: 15,
+        //                                        execute: addHash(for:))
+    }
+    
+    static func test(_: @escaping @Sendable () async -> ()) {
+        
+    }
+}
+
+
+// global actor
+@MainActor func save() {
+    print("save")
+}
+
+//Allow synchronous access to actor’s members from within itself,
+//Allow only asynchronous access to the actor’s members from any asynchronous context, and
+//Allow only asynchronous access to the actor’s members from outside the actor.
+
+
+actor HashCacheActor {
+    private(set) var hashes = [Int: String]()
+    
+    func addHash(for number: Int) async {
+        let string = SHA512.hash(data: Data(String(number).utf8)
+        ).description
+        
+        hashes[number] = string
+        
+        print(hashes)
+    }
+    
+    func compute() async {
+        //        print(Thread.current)
+        //        print("##")
+        //        self.addHash(for: 23)
+        //        self.addHash(for: 32)
+        
+        Task {
+            // print("dsdfc")
+            await self.addHash(for: 22)
+            // print("***")
+        }
+        //
+        //        print(hashes)
+        
+        //        HashCache.test {
+        //            await self.addHash(for: 22)
+        //        }
+        
+        //        DispatchQueue.concurrentPerform(iterations: 15000) { number in
+        //            self.addHash(for: number)
+        //          }
+        //        await self.addHash(for: 22)
+        
+        //        await withTaskGroup(of: Bool.self) { group in
+        //            //                    for number in 0 ... 15 {
+        //            //                        group.addTask {
+        //            //                            print(Thread.current)
+        //            //                            await self.addHash(for: number)
+        //            //                            return true
+        //            //                        }
+        //            //                    }
+        //            Task {
+        //                print(Thread.current)
+        //                self.addHash(for: 22)
+        //            }
+        //
+        ////            group.addTask {
+        ////                for number in 0 ... 15 {
+        ////                    print(Thread.current)
+        ////                    await self.addHash(for: number)
+        ////                }
+        ////
+        //                return true
+        ////            }
+        
+        
+        //        }
+        
+        // print(hashes)
+    }
+}
---
+++ Terminal/Terminal/myMain.swift
@@ -0,0 +1,44 @@
+//
+//  main.swift
+//  Terminal
+//
+//  Created by Harry Yan on 11/08/21.
+//
+
+import Foundation
+
+@main
+struct Main {
+    static func main() async throws {
+        //        let cache = HashCache()
+        //        await cache.compute()
+        //
+        let buffer = Buffer()
+        
+        //        await cache.addHash(for: 3)
+        //        await cache.compute()
+        //
+        //        print(await cache.hashes)
+        
+        
+        
+        await withTaskGroup(of: Bool.self) { group in
+            // Producer
+            for i in 0 ... 100 {
+                group.addTask() {
+                    await buffer.add(i)
+                    return true
+                }
+                
+                print("########")
+                
+                group.addTask() {
+                    await buffer.remove()
+                    return true
+                }
+                
+                print("*********")
+            }
+        }
+    }
+}
