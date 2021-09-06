81bf4710ea8e69069946cbdadff89242d89edf1c
harry yan <harryyan1238@gmail.com>
2021-09-06 21:14:34 +1200

    clean code

--- Terminal/Terminal/Future.swift
+++ Terminal/Terminal/Future.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+import UserNotifications
 
 // will be executing when u call it
 func createFuture() -> Future<Int, Never> {
@@ -19,25 +20,41 @@ func createFuture() -> Future<Int, Never> {
 // won't be executing when u call it
 // will be executing when it has subscribers
 func createDeferredFuture() -> AnyPublisher<Int, Never> {
-  return Deferred {
-    Future { promise in
-      print("Closure executed")
-      promise(.success(42))
-    }
-  }.eraseToAnyPublisher()
+    return Deferred {
+        Future { promise in
+            print("Closure executed")
+            promise(.success(42))
+        }
+    }.eraseToAnyPublisher()
 }
 
-
+extension UNUserNotificationCenter {
+    func getNotificationSettings() -> Future<UNNotificationSettings, Never> {
+        return Future { promise in
+            self.getNotificationSettings { settings in
+                promise(.success(settings))
+            }
+        }
+    }
+}
 
 func test() {
     let future = createFuture()
     
     // add subscriber
     let sub1 = future.sink(receiveValue: { value in
-      print("sub1: \(value)")
+        print("sub1: \(value)")
     }) // the Future executes because it has a subscriber
-
+    
     let sub2 = future.sink(receiveValue: { value in
-      print("sub2: \(value)")
+        print("sub2: \(value)")
     }) //  the Future executes again because it received another subscriber
+}
+
+
+@main
+struct Main2 {
+    static func main()  {
+        print("dsf")
+    }
 }
--- Terminal/Terminal/myMain.swift
+++ Terminal/Terminal/myMain.swift
@@ -8,43 +8,28 @@
 import Foundation
 import Combine
 
-
-
-
 @main
 struct Main {
     static func main() async throws {
+        let buffer = Buffer()
         
-        test()
-        
-        
-        //        let cache = HashCache()
-        //        await cache.compute()`
-        //
-        //        let buffer = Buffer()
-        
-        //        await cache.addHash(for: 3)
-        //        await cache.compute()
-        //
-        //        print(await cache.hashes)
-        
-        //        await withTaskGroup(of: Bool.self) { group in
-        //            // Producer
-        //            for i in 0 ... 100 {
-        //                group.addTask() {
-        //                    await buffer.add(i)
-        //                    return true
-        //                }
-        //
-        //                print("########")
-        //
-        //                group.addTask() {
-        //                    await buffer.remove()
-        //                    return true
-        //                }
-        //
-        //                print("*********")
-        //            }
-        //        }
+        await withTaskGroup(of: Bool.self) { group in
+            // Producer
+            for i in 0 ... 100 {
+                group.addTask() {
+                    await buffer.add(i)
+                    return true
+                }
+                
+                print("########")
+                
+                group.addTask() {
+                    await buffer.remove()
+                    return true
+                }
+                
+                print("*********")
+            }
+        }
     }
 }
