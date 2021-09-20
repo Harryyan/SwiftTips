9c041b128bf12fae922ed7aee8ad12765d96d706
harry yan <harryyan1238@gmail.com>
2021-09-21 10:06:54 +1200

    combine multicast and share

--- iOS/CombineTest.playground/Contents.swift
+++ iOS/CombineTest.playground/Contents.swift
@@ -15,7 +15,7 @@ func getPosts() -> AnyPublisher<[Post], Error> {
     }
     
     let dataPublisher = URLSession.shared.dataTaskPublisher(for: url)
-        .map { $0.data }
+        .map(\.data)
         .decode(type: [Post].self, decoder: JSONDecoder())
         .receive(on: RunLoop.main)
         .eraseToAnyPublisher()
@@ -23,34 +23,65 @@ func getPosts() -> AnyPublisher<[Post], Error> {
     return dataPublisher
 }
 
+//
+//let cancellable = getPosts().sink(receiveCompletion: {_ in }, receiveValue: {
+//    print($0)
+//})
 
-let cancellable = getPosts().sink(receiveCompletion: {_ in }, receiveValue: {
-    print($0)
-})
+///// Create timers
+///// #1 - Runloop scheduler
+//let runloop = RunLoop.main
+//let cancelable = runloop.schedule(after: runloop.now, interval: .seconds(2), tolerance: .microseconds(100)) {
+//    print("Timer Fired!")
+//}
+//
+///// #2 - Timer
+//let subscriber = Timer.publish(every: 1.0, on: .current, in: .common).autoconnect().sink {
+//    print($0)
+//}
+//
+//
+///// #3
+///// DispatchQueue
+//
+//let publisher = PassthroughSubject<Int, Never>()
+//var count = 0
+//let cancelable1 = DispatchQueue.main.schedule(after: DispatchQueue.main.now, interval: .seconds(1)) {
+//    publisher.send(count)
+//    count += 1
+//}
+//
+//let cancelable2 = publisher.sink {
+//    print($0)
+//}
+//
 
-/// Create timers
-/// #1 - Runloop scheduler
-let runloop = RunLoop.main
-let cancelable = runloop.schedule(after: runloop.now, interval: .seconds(2), tolerance: .microseconds(100)) {
-    print("Timer Fired!")
+/// share results among publishers
+guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+    fatalError("Invalid URL!")
 }
 
-/// #2 - Timer
-let subscriber = Timer.publish(every: 1.0, on: .current, in: .common).autoconnect().sink {
+var cancellables = Set<AnyCancellable>()
+let subject = PassthroughSubject<Data, URLError>()
+let request = URLSession.shared.dataTaskPublisher(for: url).map(\.data).print().multicast(subject: subject)
+request.sink(receiveCompletion: { _ in }) {
+    print("1:")
     print($0)
 }
+.store(in: &cancellables)
 
-
-/// #3
-/// DispatchQueue
-
-let publisher = PassthroughSubject<Int, Never>()
-var count = 0
-let cancelable1 = DispatchQueue.main.schedule(after: DispatchQueue.main.now, interval: .seconds(1)) {
-    publisher.send(count)
-    count += 1
-}
-
-let cancelable2 = publisher.sink {
+request.sink(receiveCompletion: { _ in }) {
+    print("2:")
     print($0)
+}
+.store(in: &cancellables)
+
+DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+    request.sink(receiveCompletion: { _ in }) {
+        print("3:")
+        print($0)
+    }
+    .store(in: &cancellables)
+    
+    request.connect().store(in: &cancellables)
 }
