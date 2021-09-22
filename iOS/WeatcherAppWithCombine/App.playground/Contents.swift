3c13c85fecc05307663105a54a1e081d01d19c08
harry yan <harryyan1238@gmail.com>
2021-09-22 21:18:56 +1200

    obserable obj

--- iOS/WeatcherAppWithCombine/App.playground/Contents.swift
+++ iOS/WeatcherAppWithCombine/App.playground/Contents.swift
@@ -5,33 +5,13 @@ import Combine
 import UIKit
 import PlaygroundSupport
 
-class WebService {
-    
-    func fetchWeather(city: String) -> AnyPublisher<Weather, Error> {
-        
-        guard let url = URL(string: URLs.weather(city: city)) else {
-           
-            fatalError("Invalid URL")
-        }
-        
-        return URLSession.shared.dataTaskPublisher(for: url)
-            .map { $0.data }
-            .decode(type: WeatherResponse.self, decoder: JSONDecoder())
-            .map { $0.main }
-            .receive(on: RunLoop.main)
-            .eraseToAnyPublisher()
-        
-    }
+class ObservableTest: ObservableObject {
+    @Published var test: String = ""
 }
-
 
 struct ContentView: View {
     @State private var username: String = ""
-    private var test = ""
-    
-    private var webservice: WebService {
-        WebService.init()
-    }
+
     
     var body: some View {
         VStack {
@@ -42,23 +22,6 @@ struct ContentView: View {
             }
             
             TextField("Username", text: $username)
-        }
-        .onAppear {
-            print("sdfdsafsd")
-            
-            // not really work
-            self.webservice.fetchWeather(city: "huston")
-                .catch { _ in
-                    Just(Weather.placeholder)
-                }
-                .map { weather in
-                    if let temp = weather.temp {
-                        return "\(temp) ℉"
-                    } else {
-                        return "Error getting weather!"
-                    }
-                }
-                .assign(to: \.username, on: self)
         }
     }
 }
